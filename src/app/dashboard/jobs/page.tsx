0ecafdd947f340685fea34b32aff1304d689cea3
lvs170603 <lvs170603@gmail.com>
2025-09-15 00:38:03 +0530

    5

--- src/app/dashboard/jobs/page.tsx
+++ src/app/dashboard/jobs/page.tsx
@@ -1,7 +1,7 @@
 
-'use client';
-
 export const dynamic = "force-dynamic";
+
+'use client';
 
 import { useState, useEffect, useCallback, useMemo } from "react";
 import { useRouter } from "next/navigation";
